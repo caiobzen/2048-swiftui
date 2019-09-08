fa4645a2c9d7b95341d9e9df9143fc18744e680a
Carlos Corrêa <caiobzen@gmail.com>
2019-09-08 16:52:04 -0300

    feat: add new project

---
+++ twofortyeight/twofortyeight/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  twofortyeight
+//
+//  Created by Carlos Corrêa on 08/09/19.
+//  Copyright © 2019 Carlos Corrêa. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
