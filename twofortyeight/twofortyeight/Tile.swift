c0ee5e11ba979e8643e86c087708016075067a47
Carlos Corrêa <caiobzen@gmail.com>
2019-09-08 16:59:22 -0300

    feat: add Tiles

---
+++ twofortyeight/twofortyeight/Tile.swift
@@ -0,0 +1,44 @@
+import SwiftUI
+
+struct Tile: View {
+    let value: Int
+    let style: TileStyle
+    var title: String {
+        value == 0 ? "" : value.description
+    }
+    
+    init(_ value: Int) {
+        self.value = value
+        style = TileStyle(value)
+    }
+    
+    var body: some View {
+        Text(title)
+            .font(.title)
+            .fontWeight(.black)
+            .padding(4)
+            .minimumScaleFactor(0.4)
+            .frame(width: 50, height: 50)
+            .background(style.backgroundColor)
+            .foregroundColor(style.foregroundColor)
+            .cornerRadius(2)
+    }
+}
+
+#if DEBUG
+struct Tile_Previews: PreviewProvider {
+    static var previews: some View {
+        Group {
+            Tile(2)
+            Tile(4)
+            Tile(8)
+            Tile(16)
+            Tile(32)
+            Tile(64)
+            Tile(128)
+            Tile(2048)
+        }
+        .previewLayout(.fixed(width: 80, height: 80))
+    }
+}
+#endif
---
+++ twofortyeight/twofortyeight/TileStyle.swift
@@ -0,0 +1,55 @@
+import SwiftUI
+
+enum TileStyle {
+    case empty
+    case two
+    case four
+    case eigth
+    case sixteen
+    case thirtyTwo
+    case sixtyFour
+    case max
+
+    init(_ value: Int) {
+        switch value {
+        case 0: self = .empty
+        case 2: self = .two
+        case 4: self = .four
+        case 8: self = .eigth
+        case 16: self = .sixteen
+        case 32: self = .thirtyTwo
+        case 64: self = .sixtyFour
+        default: self = .max
+        }
+    }
+
+    var backgroundColor: Color {
+        switch self {
+        case .empty:
+            return Color(red: 197/255, green: 186/255, blue: 175/255)
+        case .two:
+            return Color(red: 235/255, green: 228/255, blue: 220/255)
+        case .four:
+            return Color(red: 233/255, green: 223/255, blue: 204/255)
+        case .eigth:
+            return Color(red: 225/255, green: 179/255, blue: 135/255)
+        case .sixteen:
+            return Color(red: 215/255, green: 151/255, blue: 112/255)
+        case .thirtyTwo:
+            return Color(red: 211/255, green: 131/255, blue: 105/255)
+        case .sixtyFour:
+            return Color(red: 214/255, green: 113/255, blue: 85/255)
+        case .max:
+            return Color(red: 229/255, green: 204/255, blue: 132/255)
+        }
+    }
+
+    var foregroundColor: Color {
+        switch self {
+        case .two, .four:
+            return Color(red: 116/255, green: 110/255, blue: 103/255)
+        default:
+            return .white
+        }
+    }
+}
