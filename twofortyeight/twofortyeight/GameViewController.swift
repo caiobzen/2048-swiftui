61ab7e679c4602c88c231fd47e8c5595d94ac2ed
Carlos Corrêa <caiobzen@gmail.com>
2019-09-10 14:42:58 -0300

    feat: encapsulate game within a GameViewController

---
+++ twofortyeight/twofortyeight/GameViewController.swift
@@ -0,0 +1,52 @@
+import SwiftUI
+
+
+class GameViewController: UIHostingController<Board> {
+    
+    private let viewModel: BoardViewModel
+    
+    init(viewModel: BoardViewModel) {
+        self.viewModel = viewModel
+        super.init(rootView: Board(viewModel: viewModel))
+        
+        setupGestures()
+        viewModel.addNumber()
+    }
+    
+    @objc required dynamic init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setupGestures() {
+        let left = UISwipeGestureRecognizer(target: self, action: #selector(pushLeft))
+        left.direction = .left
+        let right = UISwipeGestureRecognizer(target: self, action: #selector(pushRight))
+        right.direction = .right
+        let up = UISwipeGestureRecognizer(target: self, action: #selector(pushUp))
+        up.direction = .up
+        let down = UISwipeGestureRecognizer(target: self, action: #selector(pushDown))
+        down.direction = .down
+        
+        view.addGestureRecognizer(left)
+        view.addGestureRecognizer(right)
+        view.addGestureRecognizer(up)
+        view.addGestureRecognizer(down)
+    }
+    
+    @objc func pushLeft() {
+        viewModel.push(.left)
+        viewModel.addNumber()
+    }
+    @objc func pushRight() {
+        viewModel.push(.right)
+        viewModel.addNumber()
+    }
+    @objc func pushUp() {
+        viewModel.push(.up)
+        viewModel.addNumber()
+    }
+    @objc func pushDown() {
+        viewModel.push(.down)
+        viewModel.addNumber()
+    }
+}
