4bb272f91c956800f0ff0ac5372d6c85b3900d21
Carlos Corrêa <caiobzen@gmail.com>
2019-09-11 19:21:05 -0300

    refactor: extract even more matrix functions

--- twofortyeight/twofortyeight/Extensions/ArrayExtensions.swift
+++ twofortyeight/twofortyeight/Extensions/ArrayExtensions.swift
@@ -9,25 +9,31 @@ extension Array where Iterator.Element == [Int] {
             self[row][column] = newValue
         }
     }
-    
+
     func randomIndex(for value: Int) -> (Int, Int)? {
         indexesWith(value).randomElement()
     }
-    
+
     private func indexesWith(_ value: Int) -> [(Int, Int)] {
         var indexes:[(Int, Int)] = []
-        
+
         for row in 0..<count {
-            for column in 0..<[row].count {
-                if self[row, column] == value {
-                    indexes.append((row,column))
-                }
-            }
+            indexes = indexesOf(row, with: value)
         }
-        
+
         return indexes
     }
-    
+
+    private func indexesOf(_ row: Int, with value: Int)  -> [(Int, Int)] {
+        var indexes: [(Int, Int)] = []
+        for column in 0..<[row].count {
+            if self[row, column] == value {
+                indexes.append((row,column))
+            }
+        }
+        return indexes
+    }
+
     var canCombineValues: Bool {
         guard hasZeros == false else { return true }
         for row in 0..<count {
@@ -39,21 +45,21 @@ extension Array where Iterator.Element == [Int] {
         }
         return false
     }
-    
+
     private var hasZeros: Bool {
         joined().contains(0)
     }
-    
+
     func canCombineItemAt(row: Int, column: Int) -> Bool {
         let verticallyCombinable = canCombineVertically(row: row, column: column)
         let horizontallyCombinable = canCombineHorizontally(row: row, column: column)
         return verticallyCombinable || horizontallyCombinable
     }
-    
+
     private func canCombineVertically(row: Int, column: Int) -> Bool {
         row != count - 1 && self[row][column] == self[row+1][column]
     }
-    
+
     private func canCombineHorizontally(row: Int, column: Int) -> Bool {
         column != count - 1 && self[row][column] == self[row][column+1]
     }
